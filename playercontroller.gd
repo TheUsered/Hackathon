6eb8ad2334db85b004a7d1ae15ee17c69c67412d
boospook64 <83098995+boospook64@users.noreply.github.com>
2021-04-24 14:50:41 -0400

    Add files via upload

--- playercontroller.gd
+++ playercontroller.gd
@@ -1,26 +1,43 @@
 extends KinematicBody2D
 
 var speed = 240
+var health = 50
+var damage = 10
 
 onready var anim = $animatedSprite
 
 func _process(delta):
-
+	
+	$healthtext.text = str(health)
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		anim.play("punch")
+		var coll = $"Area2D".get_overlapping_bodies()
+		for e in coll:
+			if e.has_method("damage"):
+				e.damage(damage)
+	
+	if health <= 0:
+		get_tree().quit()
 	
 	var vel = Vector2()
 	
 	if Input.is_action_pressed("ui_left"):
 		vel.x -= speed
-
+		anim.play("walk")
+		anim.flip_h = false
 	elif Input.is_action_pressed("ui_right"):
 		vel.x += speed
+		anim.play("walk")
+		anim.flip_h = true
 	elif Input.is_action_pressed("ui_up"):
 		vel.y -= speed
+		anim.play("walk")
 	elif Input.is_action_pressed("ui_down"):
 		vel.y += speed
-
+		anim.play("walk")
 	
 	move_and_slide(vel)
 
-func die():
-	Engine.time_scale = 0
+func die(d):
+	health -= d
