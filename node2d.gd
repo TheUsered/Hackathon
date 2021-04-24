a215fecc639ad5e5b9010f02ccdaddc2eb467f77
boospook64 <83098995+boospook64@users.noreply.github.com>
2021-04-24 12:12:33 -0400

    Add files via upload

--- node2d.gd
+++ node2d.gd
@@ -5,14 +5,28 @@ onready var player = preload("res://scenes/player.tscn")
 
 func _ready():
 	randomize()
-	for i in range(150 * 4):
-		randomize()
-		var position = Vector2(rand_range(-15, 15), rand_range(-8, 8))
-		tilemap.set_cellv(position, -1)
+	
+	var pos = Vector2()
+	var speed = Vector2(0, 0)
+	for i in range(400):
+		var choice = round(rand_range(1, 4))
+		if choice == 1:
+			speed = Vector2(1, 0)
+		elif choice == 2:
+			speed = Vector2(-1, 0)
+		elif choice == 3:
+			speed = Vector2(0, 1)
+		elif choice == 4:
+			speed = Vector2(0, -1)
+		
+		if pos.y >= 9 or pos.y <= -9 or pos.x >= 17 or pos.x <= -17:
+			pos = Vector2()
+			speed *= Vector2(-1, -1)
+		
+		pos += speed
+		tilemap.set_cellv(pos, -1)
 	
 	var startpos = Vector2(0, 0)
 	var p = player.instance()
 	p.position = startpos
 	add_child(p)
-	
-	
