02efbf5171757b70d2aba6040d164ebac542ebfa
boospook64 <83098995+boospook64@users.noreply.github.com>
2021-04-23 20:52:31 -0400

    Add files via upload

--- playercontroller.gd
+++ playercontroller.gd
@@ -4,6 +4,7 @@ var speed = 240
 var health = 50
 
 func _process(delta):
+	Engine.time_scale = 0
 	
 	$healthtext.text = str(health)
 	
@@ -14,12 +15,16 @@ func _process(delta):
 	
 	if Input.is_action_pressed("ui_left"):
 		vel.x -= speed
+		Engine.time_scale = 1
 	elif Input.is_action_pressed("ui_right"):
 		vel.x += speed
+		Engine.time_scale = 1
 	elif Input.is_action_pressed("ui_up"):
 		vel.y -= speed
+		Engine.time_scale = 1
 	elif Input.is_action_pressed("ui_down"):
 		vel.y += speed
+		Engine.time_scale = 1
 	
 	move_and_slide(vel)
 
