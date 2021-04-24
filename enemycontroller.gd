66cb746afc8d5a2d5de3ac1ac932c9d5b3d9d500
boospook64 <83098995+boospook64@users.noreply.github.com>
2021-04-23 20:37:29 -0400

    Add files via upload

--- enemycontroller.gd
+++ enemycontroller.gd
@@ -1,11 +1,16 @@
 extends KinematicBody2D
 
 onready var ray = $"RayCast2D"
+onready var player = $"../player"
 var vel = Vector2(0, 120)
 
 func _process(delta):
-		if ray.is_colliding():
+	if ray.is_colliding():
+		var coll = ray.get_collider()
+		if coll.has_method("die"):
+			coll.die()
+		else:
 			rotation_degrees += 180
 			vel.y *= -1
-			
-		move_and_slide(vel)
+		
+	move_and_slide(vel)
